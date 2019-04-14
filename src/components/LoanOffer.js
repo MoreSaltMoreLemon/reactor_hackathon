5f461ca17758b1bd0380149bd7879fa45a03b728
Ezra Schwepker <ezraschwepker@gmail.com>
2019-04-14 00:27:15 -0700

    added loan offers

--- src/components/LoanOffer.js
+++ src/components/LoanOffer.js
@@ -34,62 +34,50 @@ export default class LoanOffer extends Component {
     const innerHtml = { __html: this.props.offer.originator.description }
     return (
       <div className="ui segment">
-      <div className="ui three row centered grid">
-        {/* Image Representing Loan Provider (src is kinda janky, perhaps set up vars?)*/}
-        <div className="row">
-          <img
-            alt="loan originator image"
-            className="ui circular image bordered"
-            height="100px"
-            src={this.props.offer.originator.images[0].url}/>
-        </div>
-      {/* Three Column Grid Representing Middle Row on Loan Card */}
-        <div className="row">
-          <div className="ui three column centered grid">
+        <div className="ui container">
+          <div className="ui three row centered grid">
+            {/* Image Representing Loan Provider (src is kinda janky, perhaps set up vars?)*/}
             <div className="row">
-              <div className="column">
-                <div className="ui center alignedcontainer">
-                  <h2>Range</h2>
-                  <strong>${this.props.offer.maxAmount} - ${this.props.offer.minAmount}</strong>
-                </div>
-              </div>
-              <div className="column">
-                <div className="ui center aligned container">
-                  <h2>Average Monthly Price</h2>
-                  <strong>${this.props.offer.meanMonthlyPayment}</strong>
-                </div>
-              </div>
-              <div className="column">
-                <div className="ui center aligned container">
-                  <h2>Average APR</h2>
-                  <strong>{this.props.offer.meanApr}%</strong>
+              <img
+                alt="loan originator"
+                className="ui circular image bordered"
+                height="100px"
+                src={`http://${this.props.offer.originator.images[0].url}`}
+                />
+            </div>
+          {/* Three Column Grid Representing Middle Row on Loan Card */}
+            <div className="row">
+              <div className="ui three column centered grid">
+                <div className="row">
+                  <div className="column">
+                    <div className="ui center alignedcontainer">
+                      <h2>Range</h2>
+                      <strong>${this.props.offer.maxAmount} - ${this.props.offer.minAmount}</strong>
+                    </div>
+                  </div>
+                  <div className="column">
+                    <div className="ui center aligned container">
+                      <h2>Average Monthly Price</h2>
+                      <strong>${this.props.offer.meanMonthlyPayment}</strong>
+                    </div>
+                  </div>
+                  <div className="column">
+                    <div className="ui center aligned container">
+                      <h2>Average APR</h2>
+                      <strong>{this.props.offer.meanApr}%</strong>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
+            <div className="row">
+              <div className="ui container">
+                <p dangerouslySetInnerHTML={innerHtml}></p>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="ui container">
-            <p dangerouslySetInnerHTML={innerHtml}></p>
-          </div>
-        </div>
       </div>
     </div>
-
-
-
-
-    {/*  <div className="loan-offer">
-        LOAN OFFER
-        <h3>Score: {Math.floor(Math.random() * 100)}</h3>
-        <p>Only {this.props.offer.termLength} {this.props.offer.termUnit}s</p>
-        <p>Loans between {this.props.offer.maxAmount} and {this.props.offer.minAmount}</p>
-        <p>With a mean APR of {this.props.offer.meanApr}%!</p>
-        <p>And an average monthly payment of {this.props.offer.meanMonthlyPayment}</p>
-        <p dangerouslySetInnerHTML={innerHtml}></p>
-
-
-      </div> */}
     )
   }
 }
